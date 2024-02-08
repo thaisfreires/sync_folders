a9d6b35cd3287d6cd03dcc572f3d74a8e6485e43
Thais Freire <156026093+thaisfreires@users.noreply.github.com>
2024-02-08 13:14:40 +0000

    update

--- source_file.py
+++ source_file.py
@@ -1,19 +1,29 @@
 import os
 import shutil
 from datetime import datetime
-src = "source_folder"
-dst = "replica_folder"
-log_file = "log.txt"
+import argparse
 
-if not os.path.exists(src):
+def sync_folders():
+   
+   parser = argparse.ArgumentParser(description='Syncronization between two folders')
+   parser.add_argument('source_folder', help='path to the source folder')
+   parser.add_argument('replica_folder', help='path to the destination folder')
+   parser.add_argument('log_file', help='path to log file')
+
+   args = parser.parse_args()
+   src = args.source_folder
+   dst = args.replica_folder
+   log_file = args.log_file
+
+   if not os.path.exists(src):
     print("Source folder does not exist.")
     exit()
 
-if not os.path.exists(dst):
+   if not os.path.exists(dst):
     os.makedirs(dst)
     print("Replica folder created.")
 
-for item in os.listdir(src):
+   for item in os.listdir(src):
     source_path = os.path.join(src, item)
     replica_path = os.path.join(dst, item)
     
@@ -23,3 +33,5 @@ for item in os.listdir(src):
      log.write(f"{datetime.now()}: Copied {source_path} to {replica_path}\n")
     print(f'{datetime.now()}: Copied {item}\n' )
 
+if __name__ == "__main__":
+    sync_folders()
